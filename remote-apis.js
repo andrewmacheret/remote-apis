14e80553a06834debfe82eb1d3c1e31793c87cb3
Andrew Macheret <andrew.macheret@gmail.com>
2016-10-12 22:53:54 -0700

    Fix cross domain issues

--- remote-apis.js
+++ remote-apis.js
@@ -21,7 +21,7 @@ var remoteApis = settings.remoteApis;
 console.log('registering /');
 app.get('/', apicache('5 minutes'), function(req, res) {
   console.log('GET ' + req.originalUrl);
-  res.setHeader('Access-Control-Allow-Origin', 'andrewmacheret.com');
+  res.setHeader('Access-Control-Allow-Origin', 'https://andrewmacheret.com');
 
   res.set({
     'Content-Type': 'application/json'
@@ -36,7 +36,7 @@ Object.keys(remoteApis).forEach(function(api) {
 
   app.get(api, apicache('5 minutes'), function(req, res) {
     console.log('GET ' + req.originalUrl);
-    res.setHeader('Access-Control-Allow-Origin', 'andrewmacheret.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://andrewmacheret.com');
     
     // build the remote url
     var remoteUrl = remoteOptions.url;
